50cc8bb89f259732801478f251644f8fe759ea71
lab <glococo@gmail.com>
2020-04-01 11:57:30 +0100

    ttf removed from serviceworker

--- serviceworker.js
+++ serviceworker.js
@@ -1,5 +1,5 @@
 self.addEventListener('install', e=>
-  e.waitUntil( caches.open('mifitscale').then( cache=> cache.addAll(['/assets/demodata.js','/assets/mifitruler.svg','/assets/BalooThambi2-Bold.ttf','/assets/Ubuntu-Regular.ttf']) ) )
+  e.waitUntil( caches.open('mifitscale').then( cache=> cache.addAll(['/assets/demodata.js','/assets/mifitruler.svg','/assets/Ubuntu-Regular.ttf']) ) )
 )
 
 self.addEventListener('fetch', e=> e.respondWith( caches.match(e.request).then( response=> response || fetch(e.request) ) ) )
